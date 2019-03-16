02cd2010ee5f462f5cda0f2c0db568a226dac6f2
surface\camila <camila.nachbar@gmail.com>
2019-03-16 10:15:23 -0300

    initial commit

---
+++ hackatonaReprogramaApi/src/main/java/br/hackatona/reprograma/HackatonaReprogramaApiApplication.java
@@ -0,0 +1,13 @@
+package br.hackatona.reprograma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonaReprogramaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonaReprogramaApiApplication.class, args);
+	}
+
+}
